82a37d2f79edc02cb9df6a5c8ca1114bdcbecbaa
Eugene Correia <eugenecorreia@eugenes-mbp.lan>
2026-04-16 20:16:34 -0700

    wire up auth and seed demo users

--- photo-gallery/db/seeds.rb
+++ photo-gallery/db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+%w[demo1@example.com demo2@example.com demo3@example.com].each do |email|
+  User.find_or_create_by!(email_address: email) do |user|
+    user.password = "clever123"
+  end
+end
