8d33f96999d88813269bc888cacccc593fc37cdb
GSmes <garrett.smestad@gmail.com>
2026-04-22 22:50:30 -0600

    Turbo like functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,20 @@ class LikesController < ApplicationController
 
   def create
     current_user.likes.find_or_create_by(photo: @photo)
-    redirect_to root_path
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path }
+    end
   end
 
   def destroy
     current_user.likes.find_by(photo: @photo)&.destroy
-    redirect_to root_path
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path }
+    end
   end
 
   private
