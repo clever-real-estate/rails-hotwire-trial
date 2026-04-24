a134ae0a6881b3f153743f971eb22577d54b16e2
Daniel Rubio <drubio1989@gmail.com>
2026-04-24 08:21:13 -0500

    Feature 3 (Likes): Add Simplistic backend scaffolding for like functionality (pre-hotwire)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,20 @@
 class LikesController < ApplicationController
-
+  def create
+    like = Like.new(like_params)
+    if like.save
+      redirect_to root_path
+    end
+  end
+  
+  def destroy
+    like = Like.find(params[:id])
+    like.destroy if like.user_id == current_user.id
+    redirect_to root_path
+  end
+  
+  private
+  
+  def like_params
+    params.expect(like: [:user_id, :photo_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   delete "sign_out", to: "sessions#destroy"
   
   get "/photos", to: "photos#index"
+  resources :likes, only: [:create, :destroy]
   root "photos#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
