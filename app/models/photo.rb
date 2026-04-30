50c691beadc2a5f22504feae436429cbf0d52b05
Vivian Canales <vcnales@gmail.com>
2026-04-30 08:33:27 -0500

    Update liked_by method

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,6 @@ class Photo < ApplicationRecord
   has_many :likers, through: :likes, source: :user
 
   def liked_by?(user)
-    likes.where(user: user).exists?
+    likes.exists?(user: user)
   end
 end
